93dc50b23c5b79d50f8e0226b6df5f745c1bcdc4
ありす <master@16439s.jp>
2024-01-10 16:46:47 +0900

    Revert "paintボタンにしたはず..."
    This reverts commit 54f3872ffe4723737aafd426ff6f547e0022d007.

--- packages/client/src/navbar.ts
+++ packages/client/src/navbar.ts
@@ -85,12 +85,6 @@ export const navbarItemDef = reactive({
 		show: computed(() => $i != null),
 		to: "/my/clips",
 	},
-	paint: {
-		title: 'paint',
-		icon: 'fas fa-paintbrush',
-		show: computed(() => $i != null),
-		to: '/paint',	
-	},
 	channels: {
 		title: "channel",
 		icon: `${icon("ph-television")}`,
